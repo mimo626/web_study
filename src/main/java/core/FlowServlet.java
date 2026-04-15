6ffc610c62d8348d915d39a8d5ae5c5b7fba45ff
kangminju <yessuk5499@gmail.com>
2026-04-15 09:56:35 +0900

    flow 서블릿 구현

--- src/main/java/core/FlowServlet.java
+++ src/main/java/core/FlowServlet.java
@@ -12,16 +12,16 @@ public class FlowServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     public FlowServlet() {
         super();
-        System.out.println("FlowServlet 객체 생성....");
+        System.out.println("FlowServlet 객체 생성....!");
     }
 	public void init(ServletConfig config) throws ServletException {
-		System.out.println("init() 메서드 호출.....");
+		System.out.println("init() 메서드 호출.....!");
 	}
 	public void destroy() {
-		System.out.println("destroy() 메서드 호출....");
+		System.out.println("destroy() 메서드 호출....!");
 	}
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("service() 메서드 호출....");
+		System.out.println("service() 메서드 호출...!");
 	}
 
 }
