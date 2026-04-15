8b39873ddf4035c0fc7da754993c7c0dc5a6622a
kangminju <yessuk5499@gmail.com>
2026-04-15 09:57:52 +0900

    리다이렉트 서블릿 구현

--- src/main/java/core/RedirectServlet.java
+++ src/main/java/core/RedirectServlet.java
@@ -10,8 +10,8 @@ public class RedirectServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("RedirectServlet 수행");
-		response.sendRedirect("/edu/first.html"); // 절대 URI
-		//response.sendRedirect("http://www.naver.com/");
+		//response.sendRedirect("/edu/first.html"); // 절대 URI
+		response.sendRedirect("http://www.naver.com/");
 	}
 }
 
